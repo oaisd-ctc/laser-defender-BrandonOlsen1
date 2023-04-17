aef18d74237acf4092cb351d6cccccfdde9bce79
Brandon Olsen <112421088+BrandonOlsen1@users.noreply.github.com>
2023-04-17 13:55:15 -0400

    Game

--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -2,11 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Wave Congig", fileName = "New Wave Config")]
-public class WaveConfig : ScriptableObject
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject
 {
+    [SerializeField] List<GameObject> enemyPrefabs;
+
    [SerializeField] Transform pathPrefab;
    [SerializeField] float moveSpeed = 5f;
+
+
+
+    public int GetEnemyCount()
+    {
+        return enemyPrefabs.Count;
+    }
+
+    public GameObject GetEnemyPrefabs(int index)
+    {
+        return enemyPrefabs[index];
+    }
+
+
+
 
 
    public Transform GetStartingWaypoint()
